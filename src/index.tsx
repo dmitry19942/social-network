ef23ebba925c0a1c3fc09a8319e34463b378f9b1
dmitry19942 <gorustovich2013@gmail.com>
2022-07-19 20:47:36 +0300

    add github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {RootStateType} from "./redux/store";
 import ReactDOM from 'react-dom';
 import App from './App';
 import {store} from "./redux/redux-store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
 
     ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
             <App/>
             </Provider>
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root')
     );
 
