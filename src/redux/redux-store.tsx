c12a49594601417341aad534ef09dd694b64581d
dmitry19942 <gorustovich2013@gmail.com>
2022-08-12 23:42:30 +0300

    add chrome extensions

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {usersReducer} from "./users-reducer";
@@ -17,12 +17,17 @@ let rootReducer = combineReducers({
     app: appReducer
 })
 
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)
+));
+
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+// export let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 // @ts-ignore
-window.store = store;
+// window.store = store;
 
 
 
